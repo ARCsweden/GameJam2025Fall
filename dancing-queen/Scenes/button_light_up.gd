d3fe2078827578ba0ab4dca30faa0479bdf2669f
Erik <31732187+eln17011@users.noreply.github.com>
2025-10-25 18:14:07 +0200

    Light up buttons

--- dancing-queen/Scenes/button_light_up.gd
+++ dancing-queen/Scenes/button_light_up.gd
@@ -1,5 +1,5 @@
 extends GridContainer
-enum dances {
+enum Dance {
 	LEFT,
 	RIGHT,
 	UP,
@@ -9,22 +9,38 @@ enum dances {
 	ANTENNA,
 }
 
-var dances_actions = {
-	dances.LEFT: "left",
-	dances.RIGHT: "right",
-	dances.UP: "up",
-	dances.DOWN: "down",
-	dances.FLAP: "flap",
-	dances.SHAKE: "shake",
-	dances.ANTENNA: "antenna",
+var dance_actions = {
+	Dance.LEFT: "left",
+	Dance.RIGHT: "right",
+	Dance.UP: "up",
+	Dance.DOWN: "down",
+	Dance.FLAP: "flap",
+	Dance.SHAKE: "shake",
+	Dance.ANTENNA: "antenna",
+}
+
+var dance_tiles = {
+	Dance.LEFT: %Dance1,
+	Dance.RIGHT: %Dance2,
+	Dance.UP: %Dance3,
+	Dance.DOWN: %Dance4,
+	Dance.FLAP: %Dance5,
+	Dance.SHAKE: %Dance6,
+	Dance.ANTENNA: %Dance7,
 }
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta):
+	for dance in Dance.values():
+		var tile = dance_tiles.get(dance, null)
+		if tile == null:
+			push_error("Tile for " + str(dance) + " is null!")
+			continue
 		
+		if Input.is_action_pressed(dance_actions[dance]):
+			tile.color = Color("008632")  # green = active
+		else:
+			tile.color = Color("cb002f")  # white = inactive
