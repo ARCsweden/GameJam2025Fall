9b5a425253efd03cae9f77619c3a2054dde3c729
Erlatronics <erlandsson.gustav@gmail.com>
2025-10-25 09:52:46 +0200

    Tiles

--- dancing-queen/generate_map.gd
+++ dancing-queen/generate_map.gd
@@ -2,15 +2,26 @@ extends TileMapLayer
 
 @export var noise_tex : NoiseTexture2D
 
-var grass_atlas = [Vector2i(1,1)]
-var flower_atlas = [Vector2i(3,2)]
+var grass_atlas = Vector2i(1,1)
+var flower_atlas = Vector2i(3,2)
 var danger_atlas = Vector2i(2,4)
 var noise : Noise
+var width : int = 32
+var height : int = 32
+var source_id = 1
 func _ready() -> void:
 	generate_world()
 	pass
 
 func generate_world():
 	noise = noise_tex.noise
+	for x in range(-width/2, width /2):
+		for y in range(-height/2, height/2):
+			var noise_val :float = noise.get_noise_2d(x,y)
+			# placing ground
+			if noise_val >= 0.0:
+				set_cell(Vector2i(x,y), source_id, grass_atlas)
+			else:
+				set_cell(Vector2i(x,y), source_id,flower_atlas)
 	
-	pass
+		
