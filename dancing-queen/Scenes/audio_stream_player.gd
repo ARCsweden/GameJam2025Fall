9d7b2c2174b8352fd4fe5e376c52bcf90d0b91ac
Per Lenander <per.lenander@wires.se>
2025-10-26 14:19:14 +0100

    Swapped music

--- dancing-queen/Scenes/audio_stream_player.gd
+++ dancing-queen/Scenes/audio_stream_player.gd
@@ -1,13 +1,13 @@
 extends AudioStreamPlayer
 
 # Preload your two songs
-const SONG_MAIN = preload("res://Assets/Sounds/beeckround.mp3")
-const SONG_RARE = preload("res://Assets/Sounds/bumble-bee.mp3")
+const SONG_MAIN = preload("res://Assets/Sounds/bumble-bee.mp3")
+const SONG_RARE = preload("res://Assets/Sounds/beeckround.mp3")
 
 func _ready():
 	# Randomly choose a song (90% chance main, 10% rare)
 	var rand_val = randf()
-	stream = SONG_MAIN if rand_val < 0.9 else SONG_RARE
+	stream = SONG_MAIN if rand_val < 0.8 else SONG_RARE
 
 	# Play it
 	play()
